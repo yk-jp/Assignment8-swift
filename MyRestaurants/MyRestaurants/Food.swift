3fd2116125a244d8b1350ca0c5499b1c92c78d70
yk-jp <k82.y12@gmail.com>
2022-06-01 20:49:59 -0700

    display text on collection view cells

---
+++ MyRestaurants/MyRestaurants/Food.swift
@@ -0,0 +1,17 @@
+//
+//  Food.swift
+//  MyRestaurants
+//
+//  Created by Yusuke K on 2022-05-31.
+//
+
+import Foundation
+
+struct Food {
+    let name: String
+    let img:  String // URL later
+    let type: String
+    let time: [String]
+    let country: [String]
+    let cost: Double
+}
--- MyRestaurants/MyRestaurants/HomeCollectionViewController.swift
+++ MyRestaurants/MyRestaurants/HomeCollectionViewController.swift
@@ -7,82 +7,61 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
 
 class HomeCollectionViewController: UICollectionViewController {
+    private let reuseIdentifier = "Food"
+    
+    
+    private let menuItems:[Food] = [
+        Food(name: "Chipotle", img: "", type: "", time: ["Lunch", "Dinner"],country: ["Mexican"], cost: 1),
+        Food(name: "Pokeland", img: "", type: "Seafood", time: ["Lunch", "Dinner"], country: [""],cost: 2),
+        Food(name: "CTB", img: "", type: "", time: ["Breakfast", "Lunch"], country: ["American"],cost: 3),
+        Food(name: "Four Seasons", img: "", type: "", time: ["Lunch", "Dinner"], country: ["Asian", "Korean"],cost: 4),
+        Food(name: "Chick-fil-A", img: "", type: "Fast Food", time: ["Lunch"], country: ["American"],cost: 5),
+        Food(name: "Aladdins", img: "", type: "", time: ["Lunch", "Dinner"],country:["Greek","Italian"], cost: 6)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
+    
+//    private func generateLayout() -> UICollectionViewLayout {
+//       let itemSize = NSCollectionLayoutSize(
+//            widthDimension: .fractionalWidth(1.0),
+//            heightDimension: .fractionalHeight(1.0)
+//        )
+//
+//        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//
+//        let groupSize = NSCollectionLayoutSize(
+//            widthDimension: .fractionalWidth(1.0),
+//            heightDimension: .absolute(70.0)
+//        )
+//
+//        let group = NSCollectionLayoutGroup.horizontal(
+//            layoutSize: groupSize,
+//            subitem: item,
+//            count: 1
+//        )
+//    }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return menuItems.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FoodCollectionViewCell
+        
+        cell.nameLabel.text = menuItems[indexPath.item].name
     
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
+//     default -> 1
+//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
 
 }
